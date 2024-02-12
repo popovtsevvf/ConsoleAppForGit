a9fe7062da7cc195848b49c1a162555a90b2ea80
popov <popov@LAPTOP-AIT77AP7>
2024-02-12 21:03:11 +0300

    Add project files.

---
+++ ConsoleAppForGit/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppForGit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
